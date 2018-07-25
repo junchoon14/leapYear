00649c97f00fad7c0cc571fc4995ca589b8a981d
Jason Hsu <junchoon14@gmail.com>
2018-07-25 20:36:44 +0800

    version 7.25/2018

--- leapYear/ViewController.swift
+++ leapYear/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var yearText: UITextField!
+    @IBOutlet weak var yearDisplay: UILabel!
+    @IBOutlet weak var dayDisplay: UILabel!
+    @IBAction func distinguishButton(_ sender: Any) {
+        
+        let year: Int? = Int(yearText.text!)
+        
+        if let year = year {
+            if (year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0 {
+                yearDisplay.text = "LEAP"
+                dayDisplay.text = "-366 DAYS-"
+            } else {
+                yearDisplay.text = "COMMON"
+                dayDisplay.text = "-365 DAYS-"
+            }
+        } else {
+            yearText.text = ""
+            yearText.placeholder = "Please enter a number!"
+        }
+        
+        view.endEditing(true)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let date = Date()
+        let calendar = Calendar.current
+        let thisYear = calendar.component(.year, from: date)
+        if (thisYear % 4) == 0 && (thisYear % 100) != 0 || (thisYear % 400) == 0 {
+            dayDisplay.text = "-366 DAYS-"
+        } else {
+            dayDisplay.text = "-365 DAYS-"
+        }
+        yearText.placeholder = "Please enter a year"
     }
-
-
 }
 
